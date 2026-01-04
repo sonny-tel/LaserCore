2cf0d5c7d98d278bfacca204a255739145c695e6
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-01-03 22:13:23 -0800

    release changes

--- scripts/create-release-file.py
+++ scripts/create-release-file.py
@@ -8,11 +8,11 @@ assert("--version" in sys.argv)
 version_number = sys.argv[2]
 version_number_stripped_v = version_number.replace("v", "")
 
-PATH_TO_LINUX_SIG = f"./linux-artifacts/FlightCore_{version_number_stripped_v}_amd64.AppImage.tar.gz.sig"
-PATH_TO_WINDOWS_SIG = f"./windows-artifacts/bundle/msi/FlightCore_{version_number_stripped_v}_x64_en-US.msi.zip.sig"
+PATH_TO_LINUX_SIG = f"./linux-artifacts/LaserCore_{version_number_stripped_v}_amd64.AppImage.tar.gz.sig"
+PATH_TO_WINDOWS_SIG = f"./windows-artifacts/bundle/msi/LaserCore_{version_number_stripped_v}_x64_en-US.msi.zip.sig"
 
 # Text to show in update notification
-RELEASE_TEXT = "See the following link for release notes: https://github.com/R2NorthstarTools/FlightCore/releases"
+RELEASE_TEXT = "See the following link for release notes: https://github.com/R2Ion/LaserCore/releases"
 
 # Read signatures
 with open(PATH_TO_LINUX_SIG, "rt") as f:
@@ -30,11 +30,11 @@ release_dict = {
   "platforms": {
     "linux-x86_64": {
       "signature": linux_sig,
-      "url": f"https://github.com/R2NorthstarTools/FlightCore/releases/download/{version_number}/flight-core_{version_number_stripped_v}_amd64.AppImage.tar.gz"
+      "url": f"https://github.com/R2Ion/LaserCore/releases/download/{version_number}/laser-core_{version_number_stripped_v}_amd64.AppImage.tar.gz"
     },
     "windows-x86_64": {
       "signature": windows_sig,
-      "url": f"https://github.com/R2NorthstarTools/FlightCore/releases/download/{version_number}/FlightCore_{version_number_stripped_v}_x64_en-US.msi.zip"
+      "url": f"https://github.com/R2Ion/LaserCore/releases/download/{version_number}/LaserCore_{version_number_stripped_v}_x64_en-US.msi.zip"
     }
   }
 }
